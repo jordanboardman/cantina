ed417405743fa4c5dc71beb3d765eb9afdc93657
Matt Summers <mtsummersx@gmail.com>
2022-10-07 10:37:01 -0400

    properly linked css page to ejs files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Packages
+
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -8,8 +10,12 @@ const axios = require('axios').default;
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('postgres://postgres@localhost:5432/cantina');
 const { users, scores, inventories } = require('./models');
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 // -----------------------------------------------------------------------------------------------------
+// HTTP requests
+
+// Page enpoints
+
 app.get('/login', (req, res) => {
     res.render('login')
 })
@@ -37,6 +43,33 @@ app.get('/results', (req, res) => {
 app.get('/tutorial', (req, res) => {
     res.render('tutorial')
 })
-// -----------------------------------------------------------------------------------------------------
+// --------------------------------------------------
+// Table endpoints
+
+app.get('/users', async (req, res) => {
+    if(Object.keys(req.body).length != 0) { // if the request has a body, send error
+        res.statusCode = 400
+        res.send('GET requests should not have a body.')
+    }
+    else {
+        const people = await users.findAll()
+        res.send(people)
+    }
+})
+
+app.get('/scores', async (req, res) => {
+    const games = await scores.findAll({
+        order: [['points', 'DESC']] // orders all scores from highest to lowest
+    })
+    res.send(games)
+})
+
+app.get('/inventories', async (req, res) => {
+    const stuff = await inventories.findAll()
+    res.send(stuff)
+})
+// ----------------------------------------------------------------------------------------------------
+// Server
+
 port = 3000
 app.listen(port, console.log(`Server is running on port ${port}`));
