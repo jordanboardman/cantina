e8c2aee2513ec5d9902ce785efef1d3c30d10e40
Jordan <jordanboardman15@gmail.com>
2022-10-10 12:22:40 -0400

    added .listen(process.env.PORT || 5000)

--- server.js
+++ server.js
@@ -1,105 +1,103 @@
 // Packages
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const ejs = require('ejs');
-const { render } = require('ejs');
-const axios = require('axios').default;
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres@localhost:5432/cantina');
-const { users, scores, inventories } = require('./models');
-app.set('view engine', 'ejs');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const ejs = require("ejs");
+const { render } = require("ejs");
+const axios = require("axios").default;
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("postgres://postgres@localhost:5432/cantina");
+const { users, scores, inventories } = require("./models");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 // -----------------------------------------------------------------------------------------------------
 // Global Variables
 
-let username = '';
+let username = "";
 // -----------------------------------------------------------------------------------------------------
 // HTTP requests
 
 // Page endpoints
 
-app.get('/', (req, res) => {
-    res.render('login')
-})
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.post('/userlogin', async (req, res) => {
-    let user = await users.findOne({
-        where: {
-            username: req.body.username
-        }
-    })
+app.post("/userlogin", async (req, res) => {
+  let user = await users.findOne({
+    where: {
+      username: req.body.username,
+    },
+  });
 
-    if (bcrypt.compare(req.body.password, user.password)) {
-        res.redirect('/shop')
-        username = req.body.username
-    }
+  if (bcrypt.compare(req.body.password, user.password)) {
+    res.redirect("/shop");
+    username = req.body.username;
+  } else {
+    res.redirect("/");
+  }
+});
 
-    else {
-        res.redirect('/')
-    }
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
+app.post("/newuser", async (req, res) => {
+  if (req.body.password === req.body.confirmpass) {
+    const hash = await bcrypt.hash(req.body.password, 8);
+    users.create({
+      username: req.body.username,
+      password: hash,
+    });
+    inventories.create({
+      username: req.body.username,
+      day: 1,
+      veax: 0,
+      mozuc: 0,
+      zeyoc: 0,
+      gloop: 0,
+      spanu: 0,
+      credits: 2000,
+    });
+  }
+  res.redirect("/");
+});
 
-app.post('/newuser', async (req, res) => {
-    if (req.body.password === req.body.confirmpass) {
-        const hash = await bcrypt.hash(req.body.password, 8)
-        users.create({
-            username: req.body.username,
-            password: hash
-        })
-        inventories.create({
-            username: req.body.username,
-            day: 1,
-            veax: 0,
-            mozuc: 0,
-            zeyoc: 0,
-            gloop: 0,
-            spanu: 0,
-            credits: 2000
-        })
-    }
-    res.redirect('/')
-})
+app.get("/shop", async (req, res) => {
+  let inv = await inventories.findOne({
+    where: {
+      username: username,
+    },
+  });
 
-app.get('/shop', async (req, res) => {
-    let inv = await inventories.findOne({
-        where: {
-            username: username
-        }
-    })
+  res.render("shop", { inv });
+});
 
-    res.render('shop', {inv})
-})
+app.get("/brew", async (req, res) => {
+  let inv = await inventories.findOne({
+    where: {
+      username: username,
+    },
+  });
 
-app.get('/brew', async (req, res) => {
-    let inv = await inventories.findOne({
-        where: {
-            username: username
-        }
-    })
+  res.render("brew", { inv });
+});
 
-    res.render('brew', {inv})
-})
+app.get("/simulate", (req, res) => {
+  res.render("simulate");
+});
 
-app.get('/simulate', (req, res) => {
-    res.render('simulate')
-})
+app.get("/results", (req, res) => {
+  res.render("results");
+});
 
-app.get('/results', (req, res) => {
-    res.render('results')
-})
-
-app.get('/tutorial', (req, res) => {
-    res.render('tutorial')
-})
+app.get("/tutorial", (req, res) => {
+  res.render("tutorial");
+});
 // --------------------------------------------------
 // Table endpoints
 
@@ -108,69 +106,75 @@ app.get('/tutorial', (req, res) => {
 
 // ------------
 // Users GET endpoints
-app.get('/users', async (req, res) => { // GET all
-    if(Object.keys(req.body).length != 0) { // if the request has a body, send error
-        res.statusCode = 400
-        res.send('GET requests should not have a body.')
-    }
-    else {
-        const people = await users.findAll()
-        res.send(people)
-    }
-})
+app.get("/users", async (req, res) => {
+  // GET all
+  if (Object.keys(req.body).length != 0) {
+    // if the request has a body, send error
+    res.statusCode = 400;
+    res.send("GET requests should not have a body.");
+  } else {
+    const people = await users.findAll();
+    res.send(people);
+  }
+});
 
 // ------------
 // Users POST endpoint
-app.post('/users', async (req, res) => {
-    newUser = {
-        "username": req.body.username,
-        "password": req.body.password
-    }
-    if(!(newUser in users)) { // if the user doesn't already exist, add new user
-        await users.create(newUser);
-        const newEntry = await users.findOne({
-            where: {
-                "username": req.body.username}
-        })
-        res.send(newEntry)
-    }
-    else { // else send error
-        res.statusCode = 400
-        res.send('User already exists.')
-    }
-})
+app.post("/users", async (req, res) => {
+  newUser = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  if (!(newUser in users)) {
+    // if the user doesn't already exist, add new user
+    await users.create(newUser);
+    const newEntry = await users.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    res.send(newEntry);
+  } else {
+    // else send error
+    res.statusCode = 400;
+    res.send("User already exists.");
+  }
+});
 
 // -------------------------
 // Scores endpoints
 
-app.get('/scores', async (req, res) => {
-    if(Object.keys(req.body).length != 0) { // if the request has a body, send error
-        res.statusCode = 400
-        res.send('GET requests should not have a body.')
-    }
-    else {
-        const games = await scores.findAll({
-            order: [['points', 'DESC']] // orders all scores from highest to lowest
-        })
-        res.send(games)
-    }
-})
+app.get("/scores", async (req, res) => {
+  if (Object.keys(req.body).length != 0) {
+    // if the request has a body, send error
+    res.statusCode = 400;
+    res.send("GET requests should not have a body.");
+  } else {
+    const games = await scores.findAll({
+      order: [["points", "DESC"]], // orders all scores from highest to lowest
+    });
+    res.send(games);
+  }
+});
 
 // -------------------------
 // Inventories endpoints
 
-app.get('/inventories', async (req, res) => {
-    if(Object.keys(req.body).length != 0) { // if the request has a body, send error
-        res.statusCode = 400
-        res.send('GET requests should not have a body.')
-    }
-    else {
-        const stuff = await inventories.findAll()
-        res.send(stuff)
-    }
-})
+app.get("/inventories", async (req, res) => {
+  if (Object.keys(req.body).length != 0) {
+    // if the request has a body, send error
+    res.statusCode = 400;
+    res.send("GET requests should not have a body.");
+  } else {
+    const stuff = await inventories.findAll();
+    res.send(stuff);
+  }
+});
 // ----------------------------------------------------------------------------------------------------
 // Server
 
-port = 3000
-app.listen(port, console.log(`Server is running on port ${port}`));
+port = 3000;
+app.listen(
+  process.env.PORT || port,
+  console.log(`Server is running on port ${port}`)
+);
